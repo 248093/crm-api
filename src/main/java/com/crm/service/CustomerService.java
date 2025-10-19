1db2dee36d532f70437756ef1ee6ddb01ef1c9f5
2482093 <2482093410@qq.com>
2025-10-19 18:57:12 +0800

    “feat：完成客户管理列表，新增/编辑、删除部门功能”

--- src/main/java/com/crm/service/CustomerService.java
+++ src/main/java/com/crm/service/CustomerService.java
@@ -1,16 +1,28 @@
 package com.crm.service;
 
+
+import com.crm.common.result.PageResult;
 import com.crm.entity.Customer;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.crm.query.CustomerQuery;
+import com.crm.vo.CustomerVO;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.net.http.HttpResponse;
 
 /**
  * <p>
  *  服务类
  * </p>
  *
- * @author vact
+ * @author crm
  * @since 2025-10-12
  */
 public interface CustomerService extends IService<Customer> {
+    //分页
+    PageResult<CustomerVO> getPage(CustomerQuery query);
+    //导出
+    void exportCustomer(CustomerQuery query, HttpServletResponse response);
 
+    void saveOrUpdate(CustomerVO customerVO);
 }
